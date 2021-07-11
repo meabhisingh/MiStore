771f15f50875913e716f814f13d2469b66b64b57
Abhi <dude.aabhi@gmail.com>
2021-07-11 08:50:51 +0530

    Initail fix

--- src/components/NavOptios.js
+++ src/components/NavOptios.js
@@ -19,29 +19,29 @@ const NavOptios = ( {miPhones,redmiPhones,tv,laptop,fitnessAndLifeStyle,home,aud
 
    useEffect(() => {
      
-    if(window.location.pathname === "/miphones"){
+    if(window.location.pathname === "/#miphones"){
         return   setMiPhoneToggle(true)
        }
-     if(window.location.pathname === "/redmiphones"){
+     if(window.location.pathname === "/#redmiphones"){
         return   setRedmiPhoneToggle(true)
        }
-       if(window.location.pathname === "/tv"){
+       if(window.location.pathname === "/#tv"){
         return   setTvToggle(true)
        }
-       if(window.location.pathname === "/laptops"){
+       if(window.location.pathname === "/#laptops"){
         return   setLaptopToggle(true)
        }
-       if(window.location.pathname === "/lifestyle"){
+       if(window.location.pathname === "/#lifestyle"){
         return  setFitnessToggle(true)
        }
-       if(window.location.pathname === "/home"){
+       if(window.location.pathname === "/#home"){
         return  setHomeToggle(true)
        }
-       if(window.location.pathname === "/audio"){
+       if(window.location.pathname === "/#audio"){
         return  setAudioToggle(true)
        }
        
-       if(window.location.pathname === "/accessories"){
+       if(window.location.pathname === "/#accessories"){
         return  setAccessoriesToggle(true)
        }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,14 +16,14 @@ const Navbar = () => {
 
              </div>
 
-             <a  className="navlinks" href="/miphones">Mi Phones</a>
-           <a   className="navlinks"  href="/redmiphones">Redmi Phones</a>
-           <a   className="navlinks" href="/tv">TV</a>
-           <a   className="navlinks" href="/laptops">Laptops</a>
-           <a   className="navlinks" href="/lifestyle">Fitness & Lifestyle</a>
-           <a  className="navlinks" href="/home">Home</a>
-           <a   className="navlinks" href="/audio">Radio</a>
-           <a   className="navlinks" href="/accessories">Accessories</a>
+             <a  className="navlinks" href="/#miphones">Mi Phones</a>
+           <a   className="navlinks"  href="/#redmiphones">Redmi Phones</a>
+           <a   className="navlinks" href="/#tv">TV</a>
+           <a   className="navlinks" href="/#laptops">Laptops</a>
+           <a   className="navlinks" href="/#lifestyle">Fitness & Lifestyle</a>
+           <a  className="navlinks" href="/#home">Home</a>
+           <a   className="navlinks" href="/#audio">Radio</a>
+           <a   className="navlinks" href="/#accessories">Accessories</a>
 
            <div className="searchbox">
               <input type="text" name="search"  placeholder="Search Products"/>
